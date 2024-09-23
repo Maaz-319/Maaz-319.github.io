391989f102cd26d660207d47f5a220485f181378
Maaz <83403349+Maaz-319@users.noreply.github.com>
2024-09-23 18:51:31 +0500

    Created Age Calculator

---
+++ web/assets/Age_calc/script.js
@@ -0,0 +1,31 @@
+function navToggle() {
+    $('.inPage__links').slideToggle();
+}
+
+function enableButton() {
+    $('.calculate__button').prop('disabled', false);
+}
+
+function calculate() {
+    var birthDate = new Date($('.birth__date').val());
+    var currentDate = new Date();
+    let temp = '';
+    var age = currentDate.getFullYear() - birthDate.getFullYear();
+    if (currentDate.getMonth() < birthDate.getMonth() || (currentDate.getMonth() == birthDate.getMonth() && currentDate.getDate() < birthDate.getDate())) {
+        age--;
+    }
+    // calc months
+    var months = currentDate.getMonth() - birthDate.getMonth();
+    if (months < 0) {
+        months = 12 + months;
+    }
+    if (currentDate.getDate() < birthDate.getDate()) {
+        months--;
+    }
+    if (currentDate.getMonth() < birthDate.getMonth()) {
+        temp = `<br><br>• After ${12-months} months, you will be ${age+1} years old`;
+    } else {
+        temp = `<br><br>• Your birthday was ${months} months ago`;
+    }
+    $('.age__text').html(`• You are <span class="text-green-700">${age} years, ${months} months old</span><span class="text-black">${temp}</span>`);
+}
